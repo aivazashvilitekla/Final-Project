5830f280d7d3d308adb9e87e8fc438a4a096229c
tekla aivazashvili <aivazashvilitekla7@gmail.com>
2020-07-07 22:12:20 +0400

    added dropdown menu

--- script.js
+++ script.js
@@ -53,4 +53,17 @@ $(document).ready(function(){
       centerMode: true,
       focusOnSelect: true
     });
+
+    $('.first-button').on('click', function () {
+
+      $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+  
+      $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+  
+      $('.animated-icon3').toggleClass('open');
+    });
 });
