c5af6beba66c05f21e150e4b5c1399bde31a1d5c
tekla aivazashvili <aivazashvilitekla7@gmail.com>
2020-07-10 16:48:27 +0400

    almost done

--- script.js
+++ script.js
@@ -4,48 +4,17 @@ $(document).ready(function(){
       $('.nav ul').toggleClass('showing');
   });
 
-  $('.post-wrapper').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1000,
-      nextArrow: $('.next'),
-      prevArrow: $('.prev'),
-      responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 700,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 580,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-  });
-  $('#quote-carousel').carousel({
-    pause: false,
-    autoplay: true,
-    interval: 4000,
-  });
+  
   
 });
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
+  var w = window.innerWidth;
+  if(w<500){
+    document.getElementById("mySidebar").style.width = "280px";
+  }else{
+    document.getElementById("mySidebar").style.width = "400px";
+  }
+  
   document.getElementById("burger-icon").style.marginLeft = "250px";
 }
 
@@ -53,73 +22,64 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("burger-icon").style.marginLeft= "0";
 }
-// $(document).ready(function(){
-//     $('.menu-toggle').on('click', function(){
-//         $('.nav').toggleClass('showing');
-//         $('.nav ul').toggleClass('showing');
-//     });
-//     $('.carousel').carousel('pause');
-//     // $('.carousel').slick({
-//     //     slidesToShow: 3,
-//     //     slidesToScroll: 1,
-//     //     autoplay: false,
-//     //     autoplaySpeed: 1000,
-//     //     nextArrow: $('.next'),
-//     //     prevArrow: $('.prev'),
-//     //     responsive: [
-//     //         {
-//     //           breakpoint: 1024,
-//     //           settings: {
-//     //             slidesToShow: 3,
-//     //             slidesToScroll: 3,
-//     //             infinite: true,
-//     //             dots: true
-//     //           }
-//     //         },
-//     //         {
-//     //           breakpoint: 700,
-//     //           settings: {
-//     //             slidesToShow: 2,
-//     //             slidesToScroll: 2
-//     //           }
-//     //         },
-//     //         {
-//     //           breakpoint: 580,
-//     //           settings: {
-//     //             slidesToShow: 1,
-//     //             slidesToScroll: 1
-//     //           }
-//     //         }
-//     //       ]
-//     // });
 
-//     $('.one-slider').slick({
-//       slidesToShow: 1,
-//       slidesToScroll: 1,
-//       arrows: false,
-//       fade: true,
-//       asNavFor: '.slider-nav'
-//     });
-//     $('.post-wrapper').slick({
-//       slidesToShow: 3,
-//       slidesToScroll: 1,
-//       asNavFor: '.slider-for',
-//       dots: true,
-//       centerMode: true,
-//       focusOnSelect: true
-//     });
+$('#myCarousel').carousel({
+  interval: false
+});
+$('#carousel-thumbs').carousel({
+  interval: false
+});
 
-//     $('.first-button').on('click', function () {
+// handles the carousel thumbnails
+// https://stackoverflow.com/questions/25752187/bootstrap-carousel-with-thumbnails-multiple-carousel
+$('[id^=carousel-selector-]').click(function() {
+  var id_selector = $(this).attr('id');
+  var id = parseInt( id_selector.substr(id_selector.lastIndexOf('-') + 1) );
+  $('#myCarousel').carousel(id);
+});
+// Only display 3 items in nav on mobile.
+if ($(window).width() < 575) {
+  $('#carousel-thumbs .row div:nth-child(4)').each(function() {
+    var rowBoundary = $(this);
+    $('<div class="row mx-0">').insertAfter(rowBoundary.parent()).append(rowBoundary.nextAll().addBack());
+  });
+  $('#carousel-thumbs .carousel-item .row:nth-child(even)').each(function() {
+    var boundary = $(this);
+    $('<div class="carousel-item">').insertAfter(boundary.parent()).append(boundary.nextAll().addBack());
+  });
+}
+// Hide slide arrows if too few items.
+if ($('#carousel-thumbs .carousel-item').length < 2) {
+  $('#carousel-thumbs [class^=carousel-control-]').remove();
+  $('.machine-carousel-container #carousel-thumbs').css('padding','0 5px');
+}
+// when the carousel slides, auto update
+$('#myCarousel').on('slide.bs.carousel', function(e) {
+  var id = parseInt( $(e.relatedTarget).attr('data-slide-number') );
+  $('[id^=carousel-selector-]').removeClass('selected');
+  $('[id=carousel-selector-'+id+']').addClass('selected');
+});
+// when user swipes, go next or previous
+$('#myCarousel').swipe({
+  fallbackToMouseEvents: true,
+  swipeLeft: function(e) {
+    $('#myCarousel').carousel('next');
+  },
+  swipeRight: function(e) {
+    $('#myCarousel').carousel('prev');
+  },
+  allowPageScroll: 'vertical',
+  preventDefaultEvents: false,
+  threshold: 75
+});
+/*
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+*/
 
-//       $('.animated-icon1').toggleClass('open');
-//     });
-//     $('.second-button').on('click', function () {
-  
-//       $('.animated-icon2').toggleClass('open');
-//     });
-//     $('.third-button').on('click', function () {
-  
-//       $('.animated-icon3').toggleClass('open');
-//     });
-    
-// });
+$('#myCarousel .carousel-item img').on('click', function(e) {
+  var src = $(e.target).attr('data-remote');
+  if (src) $(this).ekkoLightbox();
+});
